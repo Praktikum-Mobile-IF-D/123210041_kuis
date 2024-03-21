c0cf89c2c63325791e5595a86fabba346a10d19a
Putra Jadi Mukti <putramukti26@gmail.com>
2024-03-21 15:40:22 +0700

    perbaikan

--- lib/screens/books_page.dart
+++ lib/screens/books_page.dart
@@ -9,66 +9,88 @@ class BooksPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Data Buku",
-            textAlign: TextAlign.center,
-          ),
+      appBar: AppBar(
+        title: Text(
+          "Data Buku",
+          textAlign: TextAlign.center,
         ),
-        body: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 1, crossAxisSpacing: 2, mainAxisSpacing: 1),
-          itemCount: listBuku.length,
-          itemBuilder: (context, index) {
-            return InkWell(
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return DetailPage(dataBuku: listBuku[index]);
-                }));
-              },
-              child: Card(
-                child: Row(
-                  children: [
-                    Image.network(
-                      listBuku[index].imageLink,
-                      width: 200,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          listBuku[index].title,
-                          style: TextStyle(
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 1,
+            crossAxisSpacing: 2,
+            mainAxisSpacing: 1,
+            mainAxisExtent: 250),
+        itemCount: listBuku.length,
+        padding: EdgeInsets.all(8.0),
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailPage(dataBuku: listBuku[index]);
+              }));
+            },
+            child: Card(
+              child: Row(
+                children: [
+                  Image.network(
+                    listBuku[index].imageLink,
+                    width: 200,
+                    height: double.infinity,
+                    // fit: BoxFit.cover,
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            listBuku[index].title,
+                            style: TextStyle(
                               fontWeight: FontWeight.bold,
-                              color: Colors.lightBlue),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          listBuku[index].author,
-                          style: TextStyle(
-                              fontWeight: FontWeight.normal,
-                              fontStyle: FontStyle.italic,
-                              color: Colors.grey),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          listBuku[index].pages.toString(),
-                          style: TextStyle(
-                              fontWeight: FontWeight.normal,
-                              color: Colors.grey),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
+                              color: Colors.lightBlue,
+                              fontSize: 20,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            listBuku[index].author,
+                            style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontStyle: FontStyle.italic,
+                                color: Colors.grey),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.book,
+                                color: Colors.grey,
+                              ),
+                              Text(
+                                "${listBuku[index].pages.toString()} Halaman",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            );
-          },
-        ));
+            ),
+          );
+        },
+      ),
+    );
   }
 }
--- lib/screens/detail_page.dart
+++ lib/screens/detail_page.dart
@@ -1,28 +1,63 @@
-import 'package:kuis/data_buku.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:kuis/data_buku.dart';
 
-// ignore: must_be_immutable
-class DetailPage extends StatelessWidget {
+class DetailPage extends StatefulWidget {
   DataBuku dataBuku;
 
   DetailPage({super.key, required this.dataBuku});
 
   @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(dataBuku.title),
+        title: Text(widget.dataBuku.title),
       ),
-      body: Column(
+      body: ListView(
+        padding: EdgeInsets.all(20.0),
         children: [
-          Image.network(dataBuku.imageLink),
-          Text(dataBuku.author),
-          Text(dataBuku.language),
-          Text(dataBuku.country),
-          Text(dataBuku.pages.toString()),
-          Text(dataBuku.year.toString()),
-          Text(dataBuku.isAvailable ? "Tersedia" : "Tidak tersedia"),
+          Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            Image.network(
+              widget.dataBuku.imageLink,
+              width: 150,
+            )
+          ]),
+          SizedBox(
+            height: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Penulis    : ${widget.dataBuku.author}"),
+              Text("Bahasa     : ${widget.dataBuku.language}"),
+              Text("Negara     : ${widget.dataBuku.country}"),
+              Text("Halaman    : ${widget.dataBuku.pages.toString()}"),
+              Text("Tahun      : ${widget.dataBuku.year.toString()}"),
+              Text(
+                  "Status      : ${widget.dataBuku.isAvailable ? "Tersedia" : "Tidak tersedia"}"),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: widget.dataBuku.isAvailable
+                ? () {
+                    setState(() {
+                      widget.dataBuku.isAvailable = false;
+                    });
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text("Berhasil meminjam buku"),
+                      backgroundColor: Colors.green,
+                    ));
+                  }
+                : null,
+            child: Text("Pinjam"),
+          ),
         ],
       ),
     );
